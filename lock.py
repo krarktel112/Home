3f6545d7660f87532b7c97453e6cf54295987ad5
krarktel112 <krarktel@gmail.com>
2026-03-14 14:19:10 -0400

    Update lock.py

--- lock.py
+++ lock.py
@@ -12,15 +12,15 @@ for device in devices:
     print(device) # Look for a line like 'Lock(id=CD0A1221C291)'
 
 # If you know the device ID (replace with your lock's actual ID):
-lock_id = 'FFB4AF5C4B63' 
+lock_id = 'FF:B4:AF:5C:4B:63' 
 lock = switchbot.device(id=lock_id)
 
 # Command the lock
 print(f"Status: {lock.status()}") # Query the current status
 
 # Lock the door
-lock.command('lock')
-print("Commanded to lock.")
+#lock.command('lock')
+#print("Commanded to lock.")
 
 # Unlock the door
 # lock.command('unlock')
