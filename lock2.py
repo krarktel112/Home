941b2ee34a17aef1137ea5f8fe26ac136b9d0687
krarktel112 <krarktel@gmail.com>
2026-03-14 23:39:14 -0400

    Update BLE_MAC and LOCK_MODEL for Switchbot lock

--- lock2.py
+++ lock2.py
@@ -3,10 +3,10 @@ from switchbot.discovery import GetSwitchbotDevices
 from switchbot.devices import lock
 from switchbot.const import SwitchbotModel
 
-BLE_MAC="XX:XX:XX:XX:XX:XX" # The MAC of your lock
+BLE_MAC="FF:B4:AF:5C:4B:63" # The MAC of your lock
 KEY_ID="3c" # The key-ID of your encryption-key for your lock
 ENC_KEY="7fe3283b22eee778ac4727d900972e23" # The encryption-key with key-ID "XX"
-LOCK_MODEL=SwitchbotModel.LOCK_PRO # Your lock model (here we use the Lock-Pro)
+LOCK_MODEL=SwitchbotModel.LOCK # Your lock model (here we use the Lock-Pro)
 
 
 async def main():
